ff6eda0ecd18e518a1b614f5363502b5a5b1578f
IT21218958 <it21218958@my.sliit.lk>
2023-03-01 20:00:55 +0530

    dynamic file added

---
+++ vendor/dynamicEmail.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'autoload.php';
+
+use Courier\CourierClient; // import courier client
+
+$baseUrl = "https://api.courier.com/";
+
+// create an instance of courier client by passing the Courier API base URL and the Courier Auth Token to access the Email API.
+$mail = new CourierClient($baseUrl, "YOUR-COURIER-AUTH-TOKEN");
+
+// configure the recipient information. Each channel accepts different keys
+$notificationId = "YOUR-NOTIFICATION-ID";
+
+// Identify the user by a unique ID.
+$recipient = "YOUR-RECIPIENT-ID";
+
+$emailData = null;
+
+$profile = [
+    "firstname" => "firstname",
+    "lastname" => "lastname",
+    "email" => "recipient@example.com", //email = email address of recipient
+];
+
+//convert profile array into profile object.
+$profileObject = (object) ['profile' => $profile];
+
+// Define the message content based on the event 
+$data = 'data';
+
+//convert data array into data object.
+$data = (object) ['data' => $data];
+
+try {
+    $response = $mail->sendNotification($notificationId, $recipient, null, $profileObject, $dataObject, $emailData); // send the email using Courier for the required Notification.
+    echo 'Email Sent';
+} catch (Exception $e) {
+    echo 'Caught exception: ' . $e->getMessage() . "\n";
+}
